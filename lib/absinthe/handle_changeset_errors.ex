f825a8b1821e832906ab3d794af0c55bdbe9edd4
James Florentino <james@warpgate.ventures>
2020-05-13 17:09:41 +0800

    Use option values

--- lib/absinthe/handle_changeset_errors.ex
+++ lib/absinthe/handle_changeset_errors.ex
@@ -10,7 +10,11 @@ defmodule ElixirHelpers.Absinthe.HandleChangesetErrors do
 
   defp handle_error(%Ecto.Changeset{} = changeset) do
     changeset
-    |> Ecto.Changeset.traverse_errors(fn {err, _opts} -> err end)
+    |> Ecto.Changeset.traverse_errors(fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
     |> Enum.reduce(%{}, &flatten/2)
     |> Enum.map(&translate_error/1)
   end
